46ca3fb6534e0a2670a2d47beda43b9ade8d6198
NakRex <khannasimahmad@gmail.com>
2022-01-30 17:04:41 +0530

    added middle ware and connected to server

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const config = require('./config/database');
+const PORT = process.env.PORT || 3000;
+// Connect to database
+mongoose.connect(config.database, { useNewUrlParser: true }, (err) => { 
+    console.log(err || 'Connected to database');
+});
+app.use(cors());
+app.use(express.useNewUrlParser());
+app.use(express.json());
 
+// Middleware
+app.listen(PORT, () => { 
+console.log(`Server started on port ${PORT}`);
+});
