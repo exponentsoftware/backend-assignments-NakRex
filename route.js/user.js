93c356d4e06ce801cec47cdec229d1f1151c7077
NakRex <khannasimahmad@gmail.com>
2022-02-03 23:42:20 +0530

    added routes

--- route.js/user.js
+++ route.js/user.js
@@ -2,3 +2,7 @@ const express = require('express');
 let userRouter = express.Router();
 let controller = require('../controller/user')
 userRouter.post('/register', controller.signup);
+userRouter.post('/userlogin', controller.login)
+userRouter.post('/create', controller.createTodo)
+userRouter.delete('/delete', controller.deleteTodo)
+userRouter.patch('/done', controller.done)
